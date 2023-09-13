498a20e77e493cc5531edeb141081205e55b709f
Ian Formanek <ian.formanek@gmail.com>
2023-09-13 17:00:48 +0200

    Switch Formula to use service block

--- Formula/cluvio-agent.rb
+++ Formula/cluvio-agent.rb
@@ -25,32 +25,11 @@ class CluvioAgent < Formula
     bin.install "cluvio-agent"
   end
 
-  plist_options manual: "cluvio-agent"
-
-  def plist; <<~EOS
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-    <dict>
-      <key>Label</key>
-      <string>#{plist_name}</string>
-      <key>ProgramArguments</key>
-      <array>
-        <string>#{opt_bin}/cluvio-agent</string>
-      </array>
-      <key>RunAtLoad</key>
-      <true/>
-      <key>KeepAlive</key>
-      <false/>
-      <key>WorkingDirectory</key>
-      <string>#{HOMEBREW_PREFIX}</string>
-      <key>StandardErrorPath</key>
-      <string>#{var}/log/cluvio-agent/error.log</string>
-      <key>StandardOutPath</key>
-      <string>#{var}/log/cluvio-agent/output.log</string>
-    </dict>
-    </plist>
-  EOS
+  service do
+    run opt_bin/"cluvio-agent"
+    working_dir HOMEBREW_PREFIX
+    log_path  var/"log/cluvio-agent/output.log"
+    error_log_path var/"log/cluvio-agent/error.log"
   end
 
   def caveats
