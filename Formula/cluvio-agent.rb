c3e81f725f07eb7fb41a84b3afa36dcf1742f214
Roman <roman@cluvio.com>
2024-08-09 08:36:20 +0200

    Bump version to latest release.

--- Formula/cluvio-agent.rb
+++ Formula/cluvio-agent.rb
@@ -1,24 +1,24 @@
 class CluvioAgent < Formula
   desc "Cluvio Agent is a tool that enables Cluvio to connect to databases or services running on your local machine or servers without having to expose them to the internet."
   homepage "https://github.com/cluvio/agent"
-  version "1.0.6"
+  version "1.1.0"
   license "MIT"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/cluvio/agent/releases/download/v1.0.6/cluvio-agent-1.0.6-x86_64-macos.tar.xz"
-    sha256 "9dc307121ea792f5844572005d66d266b28254109c27b2681cf02aef8fe0c5bb"
+    url "https://github.com/cluvio/agent/releases/download/v1.1.0/cluvio-agent-1.1.0-x86_64-macos.tar.xz"
+    sha256 "3c31c39df810c03d97852a7c45734dfca0a64b8d91bd3dbb0084c7abc33d9d14"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/cluvio/agent/releases/download/v1.0.6/cluvio-agent-1.0.6-aarch64-macos.tar.xz"
-    sha256 "d739c821651a4e8e5f520763c8ba11fe758ab822517d495b33d492a06ebea06c"
+    url "https://github.com/cluvio/agent/releases/download/v1.1.0/cluvio-agent-1.1.0-aarch64-macos.tar.xz"
+    sha256 "8c0dc0e07e686c48b250c5b45ba896c0298b0c90fc1c606bc92dcea58d5ab987"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/cluvio/agent/releases/download/v1.0.6/cluvio-agent-1.0.6-x86_64-linux.tar.xz"
-    sha256 "b07848c2f1e001e5d5746f9aa6fc8ad8bd7812c857fe96affb7495f8c8f670f4"
+    url "https://github.com/cluvio/agent/releases/download/v1.1.0/cluvio-agent-1.1.0-x86_64-linux.tar.xz"
+    sha256 "9e99c74b6dd59dfaa42bed8986e03861e60a195d3ccff1c0705a63c652f9adba"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/cluvio/agent/releases/download/v1.0.6/cluvio-agent-1.0.6-aarch64-linux.tar.xz"
-    sha256 "f0aa132364c5604f8a500ed07c078ad57ff5c12596d1bb0d4abdf2f401acd13a"
+    url "https://github.com/cluvio/agent/releases/download/v1.1.0/cluvio-agent-1.1.0-aarch64-linux.tar.xz"
+    sha256 "ee896939f356737263db316174b8b42b779921cd1337af1fe8b600705436dd0c"
   end
 
   def install
